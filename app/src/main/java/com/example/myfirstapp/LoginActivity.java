0f69f8650d82e8c2929b2940e5f6ad330699883e
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2019-11-02 17:09:20 -0700

    updates
    Added a scroll view and text view to the dashboard page. Hid the action bar. Changes done by Karishma.

--- app/src/main/java/com/example/myfirstapp/LoginActivity.java
+++ app/src/main/java/com/example/myfirstapp/LoginActivity.java
@@ -26,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
